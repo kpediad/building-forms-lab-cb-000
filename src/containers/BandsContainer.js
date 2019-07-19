a83263825c31aa2b987f6a9c668187fc7afefc74
Konstantinos Pediaditakis <pediadk@gmail.com>
2019-07-19 15:00:46 +0000

    Automatically backed up by Learn

--- src/containers/BandsContainer.js
+++ src/containers/BandsContainer.js
@@ -1,13 +1,24 @@
-import React, { Component } from 'react'
-
-class BandsContainer extends Component {
+import React, { Component } from 'react';
+import { connect } from 'react-redux'
+import Todo from './Todo'
+ 
+class TodosContainer extends Component {
+ 
+  renderTodos = () => this.props.todos.map((todo, id) => <Todo key={id} text={todo} />)
+ 
   render() {
     return(
       <div>
-        BandsContainer
+        {this.renderTodos()}
       </div>
-    )
+    );
+  }
+};
+ 
+const mapStateToProps = state => {
+  return {
+    todos: state.todos
   }
 }
-
-export default BandsContainer
+ 
+export default connect(mapStateToProps)(TodosContainer);
