bc53932e664a59b1656de7687015932b7b931ddf
Konstantinos Pediaditakis <pediadk@gmail.com>
2019-07-19 14:58:49 +0000

    Automatically backed up by Learn

--- src/components/BandInput.js
+++ src/components/BandInput.js
@@ -15,7 +15,11 @@ class BandInput extends Component {
     handleSubmit = event => {
         event.preventDefault();
         this.props.dispatch({ type: 'ADD_BAND', payload: this.state });
+        this.setState({
+          name: '',
+        })
     };
+
 
     render() {
         return (
